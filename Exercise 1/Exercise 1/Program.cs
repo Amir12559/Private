c2de52786c5f46b64d7030b7f8388f631ec655fc
Amir12559 <38102047+Amir12559@users.noreply.github.com>
2018-04-06 15:04:49 +0200

    School work

---
+++ Exercise 1/Exercise 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+
+            //Console.WriteLine();
+            //var test = "Hello";
+            
+            //string test2 = "Hello";
+            //test = 10.ToString();
+            Console.ReadKey();
+            //String intTest = "10a";
+            // int intvalue = int.Parse(int test);
+        }
+    }
+}
---
+++ calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable deklaration
+            Double FirstNum;
+            Double SecondNum;
+            Double Answer;
+            string operation;
+
+
+            Console.WriteLine("This is my first calculater");
+ 
+
+            Console.Write(" Enter first no ");
+            FirstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" Enter Second no ");
+            SecondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your operation ( * , / , +, -) ");
+            operation = Console.ReadLine();
+
+            if (operation == "*")
+            {
+                Answer = FirstNum * SecondNum;
+                Console.WriteLine(FirstNum + "*" + SecondNum + " = " + Answer);
+                Console.ReadLine();
+            }
+            else if (operation == "/")
+            {
+                Answer = FirstNum / SecondNum;
+                Console.WriteLine(FirstNum + "/" + SecondNum + " = " + Answer);
+                Console.ReadLine();
+            }
+            else if (operation == "+")
+            {
+                Answer = FirstNum + SecondNum;
+                Console.WriteLine(FirstNum + " + " + SecondNum + " = " + Answer);
+                Console.ReadLine();
+            }
+            else if (operation == "-")
+            {
+                Answer = FirstNum - SecondNum;
+                Console.WriteLine(FirstNum + " - " + SecondNum + " = " + Answer);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Sorry that is not correct format! Please restart!");     //Catch
+                Console.ReadLine();
+            }
+        }
+    }
+}
